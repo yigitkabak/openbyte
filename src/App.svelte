8428e0bc67c0629da40c0e77b01924f231c45b3a
Yiğit KABAK <yigitkabak@tuta.io>
2025-07-23 11:30:52 +0300

    Create App.svelte

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import Css from './scripts/css.svelte';
+</script>
+
+<div class="container">
+  <div class="header">
+    <h1>openbyte</h1>
+  </div>
+ <div class="subheader">
+    <p>The only hub for <a href="https://github.com/yigitkabak/huh">.huh</a>, <a href="https://github.com/yigitkabak/aperium">aperium</a> and many more projects! a product of yigit.</p>
+  </div>
+<nav>
+    <a href="https://github.com/yigitkabak" class="nav-link">github</a>
+  </nav>
+<footer>
+    <p>a yigit production.</p>
+  </footer>
+</div>
+
+<Css />
