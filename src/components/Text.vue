c97951709b879cdf271302b8dbd2b920f920b900
Yiğit KABAK <yigitkabak@tuta.io>
2025-08-04 20:00:21 +0300

    Create Text.vue

---
+++ src/components/Text.vue
@@ -0,0 +1,9 @@
+<template>
+  <p>The only hub for <a href="https://github.com/yigitkabak/aperium">aperium</a> and many more projects! a product of yigit.</p>
+</template>
+
+<script>
+export default {
+  name: 'BaseText'
+}
+</script>
