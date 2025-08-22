3558976b73cd52b2a39983ca098919e1e711716e
Yiğit KABAK <yigitkabak@tuta.io>
2025-08-22 16:59:12 +0300

    Update Text.vue

--- src/components/Text.vue
+++ src/components/Text.vue
@@ -1,5 +1,5 @@
 <template>
-  <p>The only hub for <a href="https://github.com/yigitkabak/aperium">aperium</a> and many more projects! a product of yigit.</p>
+  <p>The only hub for <a href="https://github.com/yigitkabak/aperium">aperium</a>, <a href="https://github.com/yigitkabak/Fenrir">Fenrir</a> and many more projects! a product of yigit.</p>
 </template>
 
 <script>
